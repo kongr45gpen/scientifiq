50b2aa2c0237629a4a23c9717d7a2a2c75b56bb8
Konstantinos Kanavouras <1204-kkanavouras@users.noreply.gitlab.uni.lu>
2022-07-16 02:03:39 +0200

    Initial commit

---
+++ src/main/kotlin/com/kongr45gpen/scientifiq/ScientifiqApplication.kt
@@ -0,0 +1,12 @@
+package com.kongr45gpen.scientifiq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class ScientifiqApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScientifiqApplication>(*args)
+}
---
+++ src/test/kotlin/com/kongr45gpen/scientifiq/ScientifiqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kongr45gpen.scientifiq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScientifiqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
